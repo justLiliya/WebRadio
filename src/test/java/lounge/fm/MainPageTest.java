b3b7e33266ec259c67e3266687aae3ef6ffa28be
Lil <liliapaper@gmail.com>
2022-10-03 22:12:51 +0300

    Added parametrized tests

--- src/test/java/lounge/fm/MainPageTest.java
+++ src/test/java/lounge/fm/MainPageTest.java
@@ -1,11 +1,19 @@
 package lounge.fm;
+
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.ElementsCollection;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.WebDriverConditions.url;
 
 
 public class MainPageTest {
@@ -14,11 +22,9 @@ public class MainPageTest {
     public static void setUp() {
         Configuration.browserSize = "1920x1080";
     }
-    //TODO Сделать параметризацию, принимать на вход: число - элемент массива(для гет[]), текст, который нужно проверять, оригинальный элемен страницы перехода
-    //TODO{0, "EASY GROOVES", локатор элемента}
 
     @Test
-    public void CheckElementsOfMainPage() {
+    public void CheckHederMainPage() {
         open("http://loungefm.com.ua/");
         $("#index").shouldHave(text("Lounge Fm"));
         $("#terrace").shouldHave(text("Terrace"));
@@ -27,15 +33,44 @@ public class MainPageTest {
         $("#podcasts").shouldHave(text("Podcasts"));
         $("#navbarDropdown").shouldHave(text("Radioshow")).click();
         ElementsCollection a = $$x("//a[@class='dropdown-item']");
-                a.get(0).shouldHave(text("EASY GROOVES"));
-                a.get(1).shouldHave(text("Chill Out Essentials"));
-                a.get(2).shouldHave(text("Buddha Bar"));
-                a.get(3).shouldHave(text("Deep Inside By Timo"));
+        a.get(0).shouldHave(text("EASY GROOVES"));
+        a.get(1).shouldHave(text("Chill Out Essentials"));
+        a.get(2).shouldHave(text("Buddha Bar"));
+        a.get(3).shouldHave(text("Deep Inside By Timo"));
         $("#about").shouldHave(text("About"));
         $("#contacts").shouldHave(text("Contacts"));
         $(".img[src='img/logo.png']").isDisplayed();
-        $x("//img[@src='img/sliders/carousel_donate/fm_banner.jpg']").click();
+    }
 
+    @CsvSource(value = {
+            "0, http://loungefm.com.ua/grooves",
+            "1, http://loungefm.com.ua/essentials",
+            "2, http://loungefm.com.ua/buddha",
+            "3, http://loungefm.com.ua/timo"
+    })
+    @ParameterizedTest(name = " Проверка перехода по пунктам выпадающего меню Радиошоу {0}")
+    public void CheckTransitionsInDropdownMenuItems(String arrayNumber, String testdata) {
+        open("http://loungefm.com.ua/");
+        $("#navbarDropdown").shouldHave(text("Radioshow")).click();
+        ElementsCollection a = $$x("//a[@class='dropdown-item']");
+        a.get(Integer.parseInt(arrayNumber)).click();
+        webdriver().shouldHave(url(testdata));
+    }
+
+
+    static Stream<Arguments> goToTheAppsStore() {
+        return Stream.of(
+                Arguments.of((".//a[@title='Официальное приложение Lounge FM для Android']"),
+                        Arguments.of(".//a[@title='Официальное приложение Lounge FM для iOS']")
+                ));
+    }
+
+    @MethodSource
+    @ParameterizedTest(name = "Переход в аппстор - {0}")
+    public void goToTheAppsStore(String buttons) {
+        open("http://loungefm.com.ua/");
+        $x(buttons).click();
+        switchTo().window(1);
     }
 }
 
